0af8437b92b018de02d1e298f463e9563c01e3f8
msorelli <sorelli@lens.unifi.it>
2023-09-11 15:42:19 +0200

    Do not blacken uncorrected image channels

--- cidre/correction.py
+++ cidre/correction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a2ce73426a3f12072d3b3a0b135681f161953dc38bbbc9614e04ffe138c48e
-size 7620
+oid sha256:b3193d40a685b8cf5adac8470cf6a7d9d9e9cf39389d8457bbca324eec809e96
+size 7727
